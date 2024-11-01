8457d462b9d7094a350a48e7a3a65642f3e983d4
Shaheed mon <sheheedk07@gmail.com>
2024-11-01 10:38:11 +0530

    Time: 0 ms (100%), Space: 49.1 MB (21.08%) - LeetHub

---
+++ 2556-convert-the-temperature/2556-convert-the-temperature.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+  let  Kelvin = celsius + 273.15
+let Fahrenheit = celsius * 1.80 + 32.00
+const ans =[Kelvin,Fahrenheit ]
+return ans
+};
