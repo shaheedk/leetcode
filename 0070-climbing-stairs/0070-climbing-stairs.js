67de6de5ded304b0338f462d7f17e878f04a54c8
Shaheed mon <sheheedk07@gmail.com>
2024-11-10 14:13:56 +0530

    Time: 51 ms (5.19%), Space: 47.7 MB (99.11%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+function climbStairs(n) {
+    if (n === 1) {
+        return 1;
+    }
+    if (n === 2) {
+        return 2;
+    }
+
+    let first = 1;
+    let second = 2;
+
+    for (let i = 3; i <= n; i++) {
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return second;
+}
