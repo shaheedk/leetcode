9f58df4cde869e970cd41a8ba6cf40d2bd8e9794
Shaheed mon <sheheedk07@gmail.com>
2024-11-10 11:40:21 +0530

    Time: 91 ms (7.15%), Space: 53 MB (40.63%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  
+    const array = Array.from(String(x), Number).reverse()
+const number = parseInt(array.join('')) * Math.sign(x);
+  if(number > 2147483647 || number < -2147483648){
+return 0
+    }else{
+        return number
+    }
+};
