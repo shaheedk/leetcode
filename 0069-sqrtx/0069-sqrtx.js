9dae008c2ad36b29eabe53b46f13cd4c67c6ee1b
Shaheed mon <sheheedk07@gmail.com>
2024-11-11 23:17:04 +0530

    Time: 26 ms (16.6%), Space: 52.1 MB (86.69%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+if (x < 0) return -1; 
+    let result = 0;
+
+    for (let i = 0; i * i <= x; i++) {
+        result = i; 
+    }
+
+    return result;
+
+};
