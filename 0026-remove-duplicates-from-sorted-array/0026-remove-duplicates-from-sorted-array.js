31d888818b7bc48fcc5b3a21121b72196816bbf6
Shaheed mon <sheheedk07@gmail.com>
2024-11-13 13:13:59 +0530

    Time: 0 ms (100%), Space: 51.6 MB (95.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-   let x=1
+   let x=1  
    for(i=0;i<nums.length-1;i++){
     if(nums[i] != nums[i+1]){
         nums[x]=nums[i+1]
