5f3c3a0eb61e20120c599144ae4d76ccaea3e628
Shaheed mon <sheheedk07@gmail.com>
2024-11-14 15:53:52 +0530

    Time: 1 ms (83.67%), Space: 53.3 MB (42%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+ for(i=num.length-1;i>=0;i--){
+       if(num[i]%2==1){
+           return num.substring(0, i + 1);
+       }
+    }
+    return ""
+};
