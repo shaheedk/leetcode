27cc36919ae098707a84aca2e95cf0951dd5f692
Shaheed mon <sheheedk07@gmail.com>
2024-11-16 10:58:49 +0530

    Time: 5 ms (53.18%), Space: 51.2 MB (92.56%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+const  frequency={}
+     const majorityCount = Math.floor(nums.length / 2);
+     for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        frequency[num] = (frequency[num] || 0) + 1;
+
+        if (frequency[num] > majorityCount) {
+            return num; // Majority element found
+        }
+    }
+  return null; // No majority element
+}
+
