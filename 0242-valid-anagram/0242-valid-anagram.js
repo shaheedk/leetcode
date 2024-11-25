f50a0c2bc75fb5fe647472ffc5e41fe7e6f06b34
Shaheed mon <sheheedk07@gmail.com>
2024-11-25 22:45:47 +0530

    Time: 31 ms (24.2%), Space: 54 MB (40.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+       if (s.length !== t.length) {
+        return false;
+    }
+
+    // Step 2: Sort and compare
+    let sortedStr1 = s.split('').sort().join('');
+    let sortedStr2 = t.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
+
+};
