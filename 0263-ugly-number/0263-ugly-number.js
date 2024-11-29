ceef75f8957aab316710a49174f4c98f64439091
Shaheed mon <sheheedk07@gmail.com>
2024-11-29 22:18:03 +0530

    Time: 0 ms (100%), Space: 52.8 MB (30.14%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    if (n <= 0) return false;  // Negative numbers and 0 are not ugly numbers
+    while (n % 2 === 0) n /= 2;  // Divide by 2 as long as it's divisible by 2
+    while (n % 3 === 0) n /= 3;  // Divide by 3 as long as it's divisible by 3
+    while (n % 5 === 0) n /= 5;  // Divide by 5 as long as it's divisible by 5
+    return n === 1;
+};
