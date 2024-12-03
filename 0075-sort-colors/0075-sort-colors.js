adac103e8f19d650b4e1d77bc7d0e4117d341d55
Shaheed mon <sheheedk07@gmail.com>
2024-12-03 16:51:42 +0530

    Time: 2 ms (19.49%), Space: 49.5 MB (32.3%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,5 +3,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    return nums.sort()
+   for (let i = 1; i < nums.length; i++)
+    for (let j = 0; j < i; j++)
+        if (nums[i] < nums[j]) {
+            let x = nums[i];
+            nums[i] = nums[j];
+            nums[j] = x;
+        }
+        return nums
 };
