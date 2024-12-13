3d88b91dc9326350496036eee55db3dbdab166a2
Shaheed <sheheedk07@gmail.com>
2024-12-13 18:08:10 +0530

    Time: 3 ms (97.73%), Space: 50.5 MB (74.73%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+function isIsomorphic(s, t) {
+    if (s.length !== t.length) return false;
+
+    const mapS = {};
+    const mapT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        let charS = s[i];
+        let charT = t[i];
+
+        // If there is already a mapping in mapS for charS, check if it matches charT
+        if (mapS[charS] !== undefined) {
+            if (mapS[charS] !== charT) {
+                return false; // Mapped to different character
+            }
+        } else {
+            mapS[charS] = charT; // Set mapping from charS to charT
+        }
+
+        // If there is already a mapping in mapT for charT, check if it matches charS
+        if (mapT[charT] !== undefined) {
+            if (mapT[charT] !== charS) {
+                return false; // Mapped to different character
+            }
+        } else {
+            mapT[charT] = charS; // Set mapping from charT to charS
+        }
+    }
+
+    return true;
+}
+
+
