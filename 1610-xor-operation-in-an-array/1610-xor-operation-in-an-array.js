1ef27e23d9c3a2090597bf6f34d650dd5394735d
Shaheed <sheheedk07@gmail.com>
2024-12-16 17:01:42 +0530

    Time: 0 ms (100%), Space: 49.2 MB (18.71%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+var xorOperation = function(n, start) {
+
+	let result = start;
+
+	for (let i = 1; i < n; i++) {
+		result ^= start + 2 * i;
+	}
+
+	return result;
+
+};
