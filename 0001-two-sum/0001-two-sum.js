d4702d67236fd6f76867d93e1feed67679a51685
Shaheed <sheheedk07@gmail.com>
2025-01-08 17:13:08 +0530

    0001-two-sum.js

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,8 +1,5 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+
+
 var twoSum = function(nums, target) {
       for (i=0;i<nums.length;i++){
 for(j=i+1;j<nums.length;j++){
@@ -11,5 +8,6 @@ for(j=i+1;j<nums.length;j++){
     }
 }
       }
+    
       return []
 };
