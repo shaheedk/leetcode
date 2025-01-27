10612cefef119a1ddf15ea55eb993937f9d5f34a
Shaheed <sheheedk07@gmail.com>
2025-01-27 14:07:17 +0530

    0041-first-missing-positive.js

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -6,6 +6,7 @@ var firstMissingPositive = function(nums) {
     nums=nums.sort((a,b)=>a-b).filter((e)=>e>0)
       nums=[...new Set(nums)]
        for(i=0;i<nums.length;i++){
+           
         if(nums[i]!=i+1){
             return i+1
         }
