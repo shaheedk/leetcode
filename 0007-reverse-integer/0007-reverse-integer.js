6babfcd130140952f9e8d95db168b0607052e193
Shaheed <sheheedk07@gmail.com>
2025-02-08 22:16:52 +0530

    0007-reverse-integer.js

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var reverse = function(x) {
-  
     const array = Array.from(String(x), Number).reverse()
 const number = parseInt(array.join('')) * Math.sign(x);
   if(number > 2147483647 || number < -2147483648){
