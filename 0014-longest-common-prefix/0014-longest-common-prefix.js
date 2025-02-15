f722d2c428ff8f07302757f016ae75906d7dd278
Shaheed <sheheedk07@gmail.com>
2025-02-15 09:41:55 +0530

    0014-longest-common-prefix.js

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -5,7 +5,7 @@
 var longestCommonPrefix = function(strs) {
     
     if (strs.length === 0) return "";
-    
+       
     for (let i = 0; i < strs[0].length; i++) {
         for (let j = 1; j < strs.length; j++) {
             if (strs[j].length <= i || strs[j][i] !== strs[0][i]) {
